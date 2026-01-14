bd3a47ab1908f265784c8a25c319e78d6fc0c161
Mia Boulter <miaboulter@outlook.com>
2026-01-14 19:42:06 +0000

    Update to .net 10

---
+++ GrayscaleInverted/GrayscaleInvertedHeatFilter.cs
@@ -0,0 +1,15 @@
+﻿using ReversePack.PluginCore;
+
+namespace GrayscaleInverted
+{
+    public class GrayscaleInvertedHeatFilter : IHeatFilter
+    {
+        public string DisplayName => "Grayscale Inverted";
+
+        public Color ApplyTo(double mapValue)
+        {
+            byte part = (byte)(255 - (mapValue * 255));
+            return new Color(part, part, part);
+        }
+    }
+}
